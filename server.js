a5b0cdb2eb814549cd8e226b1933a13dd874f2f2
MailyOstios <ostiosmaily39@gmail.com>
2026-05-03 21:31:55 -0500

    fix: cors y rutas

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 server.use(middlewares)
 server.use(router)
 
